63b49a5443c8107235ecf9fe5863d033d534399f
gilberto-009199 <gilberto.oliveira@aluno.faculdadeimpacta.com.br>
2023-08-19 00:38:45 +0000

    adding param APP_NAME for index logs

--- dist/index.js
+++ dist/index.js
@@ -5,5 +5,4 @@ let dotenv = require('dotenv');
 dotenv.config({ path: './.env' });
 const logs_initializer_1 = require("./log/logs_initializer");
 Object.defineProperty(exports, "getLogger", { enumerable: true, get: function () { return logs_initializer_1.getLogger; } });
-(0, logs_initializer_1.getLogger)().info("[data]");
 //# sourceMappingURL=index.js.map
